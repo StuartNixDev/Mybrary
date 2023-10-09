8bc93d7f691c494a47765bb9d17f0bdaf38b9ca5
Stuart Nicholson <stuartnix@hotmail.co.uk>
2023-10-09 08:56:15 +0100

    Author Routes added

--- server.js
+++ server.js
@@ -5,14 +5,17 @@ if (process.env.NODE_ENV !== "production") {
 const express = require("express");
 const app = express();
 const expressLayouts = require("express-ejs-layouts");
+const bodyParser = require("body-parser");
 
 const indexRouter = require("./routes/index");
+const authorRouter = require("./routes/authors");
 
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 app.set("layout", "layouts/layout"); //Sets default layout for multiple pages called 'layout'
 app.use(expressLayouts);
 app.use(express.static("public")); // public files. Javascript, stylesheets, images etc in folder called 'public'
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: false }));
 
 const mongoose = require("mongoose");
 mongoose.connect(process.env.DATABASE_URL);
@@ -22,4 +25,6 @@ db.on("error", (error) => console.error(error));
 db.once("open", () => console.log("Connected to Mongoose"));
 
 app.listen(process.env.PORT || 3000); //First part allows the live server to tell us the port to listen too. For Dev we use 3000
+
 app.use("/", indexRouter);
+app.use("/authors", authorRouter);
